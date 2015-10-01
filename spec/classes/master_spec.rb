a3a48cddf323a0a327797c5534152664d9c4777a
Chris Tessmer <chris.tessmer@onyxpoint.com>
2015-10-01 12:21:07 -0400

    (SIMP-353) Freshen up spec tests + use_fips logic
    Before this commit, the use_fips logic required two variables and could
    not be influenced with a parameter.  The module was also missing some
    support files and used old versions of others.   This commit updates the
    use_fips logic and modernizes the module structure to include better
    support files (e.g., Gemfile, metadata.json) and current spec test
    practices (e.g., expectations, simp-rspec-puppet-facts).
    
    SIMP-353 #comment updated use_fips logic,
    SIMP-353 #comment added pupmod::use_fips parameter
    SIMP-353 #comment freshened up module practices (files, tests)
    SIMP-353 #comment example of trusted node data spec tests for 3.x & 4.x
    SIMP-353 #close

--- spec/classes/master_spec.rb
+++ spec/classes/master_spec.rb
@@ -1,70 +1,35 @@
 require 'spec_helper'
 
 describe 'pupmod::master' do
-  base_facts = {
-    "RHEL 6" => {
-      :apache_version => '2.2',
-      :fqdn => 'spec.test',
-      :grub_version => '0.97',
-      :hardwaremodel => 'x86_64',
-      :init_systems => ['sysv','rc','upstart'],
-      :interfaces => 'lo,eth0',
-      :ipaddress => '1.2.3.4',
-      :ipaddress_eth0 => '1.2.3.4',
-      :ipaddress_lo => '127.0.0.1',
-      :lsbmajdistrelease => '6',
-      :operatingsystem => 'RedHat',
-      :operatingsystemmajrelease => '6',
-      :passenger_root => '/usr/lib/ruby/gems/1.8/gems/passenger',
-      :passenger_version => '4',
-      :processorcount => 4,
-      :selinux_current_mode => 'enabled',
-      :trusted => { 'certname' => 'spec.test' },
-      :uid_min => '500',
-      :operatingsystemrelease => '6',
-      :osfamily => 'RedHat',
-      :use_fips => true
-    },
-    "RHEL 7" => {
-      :apache_version => '2.4',
-      :fqdn => 'spec.test',
-      :grub_version => '2.02~beta2',
-      :hardwaremodel => 'x86_64',
-      :init_systems => ['sysv','rc','upstart'],
-      :ipaddress => '1.2.3.4',
-      :interfaces => 'lo,eth0',
-      :ipaddress_eth0 => '1.2.3.4',
-      :ipaddress_lo => '127.0.0.1',
-      :lsbmajdistrelease => '7',
-      :operatingsystem => 'RedHat',
-      :operatingsystemmajrelease => '7',
-      :passenger_root => '/usr/lib/ruby/gems/1.8/gems/passenger',
-      :processorcount => 4,
-      :passenger_version => '4',
-      :selinux_current_mode => 'enabled',
-      :trusted => { 'certname' => 'spec.test' },
-      :uid_min => '500',
-      :operatingsystemrelease => '7',
-      :osfamily => 'RedHat',
-      :use_fips => true
-    }
-  }
+    on_supported_os.each do |os, os_facts|
+      context "on #{os}" do
+        let(:trusted_facts){{
+          'certname' => 'spec.test'
+        }}
+        let(:base_facts) {{
+            :selinux_current_mode => 'enabled',
+            :selinux              => true,
+        }}
+        let(:facts){
+          x = os_facts.merge(base_facts)
+          x[:trusted] = trusted_facts if Puppet.version < "4.0.0"
+          x
+        }
+        if Puppet.version >= "4.0.0"
+          let(:trusted_data){ trusted_facts }
+        end
 
-  shared_examples_for "a fact set master" do
-    it { should compile.with_all_deps }
-    it { should create_class('apache') }
-    it { should create_class('pupmod') }
-    it { should create_class('pupmod::master') }
-    it { should create_class('pupmod::master::base') }
-  end
+      shared_examples_for "a fact set master" do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('apache') }
+        it { is_expected.to create_class('pupmod') }
+        it { is_expected.to create_class('pupmod::master') }
+        it { is_expected.to create_class('pupmod::master::base') }
+      end
 
-  describe "RHEL 6" do
-    it_behaves_like "a fact set master"
-    let(:facts) {base_facts['RHEL 6']}
-  end
-
-  describe "RHEL 7" do
-    it_behaves_like "a fact set master"
-    let(:facts) {base_facts['RHEL 7']}
+      describe "with default parameters" do
+        it_behaves_like "a fact set master"
+      end
+    end
   end
 end
