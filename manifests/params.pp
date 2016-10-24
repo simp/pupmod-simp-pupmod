a3978e15f005344217eec89a2dfbf8a797b556e7
Liz Nemsick <lnemsick.simp@gmail.com>
2016-10-24 15:47:06 -0400

    (SIMP-1675) pupmod::ssldir breaks auditd restart
    Fixed the following:
    * Expanded pupmod::ssldir so that the puppet-specific, '$vardir'
      string no longer appears in the puppet_master audit rule.
    * Fixed a bug that resulted in a malformed directory setting for the
      gem-home parameter in puppetserver.conf.
    
    Also improved test coverage.
    
    SIMP-1675 #close

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,14 +2,21 @@
 #
 # A set of defaults for the 'pupmod' namespace
 #
+# [*vardir*]
+# Type: Absolute Path
+# Default: /var/lib/puppet
+#
 # $java_max_memory
 #   should not exceed 12G (SIMP-1128)
 #
-class pupmod::params {
+class pupmod::params (
+  $vardir               = '/var/lib/puppet'
+) {
   if ($::memorysize_mb * 0.8) >= 12000 {
     $java_max_memory = '12g'
   }
   else {
     $java_max_memory = '80%'
   }
+  $ssldir = "${vardir}/ssl"
 }
