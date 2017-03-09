c0a1f04f6a27da8679b5f45f2e3f851bb9607845
Trevor Vaughan <tvaughan@onyxpoint.com>
2017-03-08 19:29:36 -0500

    Phrasing

--- manifests/agent/cron.pp
+++ manifests/agent/cron.pp
@@ -1,69 +1,81 @@
-# This class configures the cron settings for a non-daemonized puppet
-# client.
+# This class configures the cron settings for a non-daemonized puppet client
 #
 # @param interval
-#   The cron iteration time (in minutes) for running puppet.
-#   This applies the standard */$interval style syntax from cron.
-#   See crontab(5) for additional details.
+#   The cron iteration time (in minutes) for running puppet
 #
-#   Note: This is overridden if $minute is set to anything other
-#   than 'nil'.  If this is the case, it is assumed that you want finer
-#   control over your puppet run.
+#   * This applies the standard ``*/$interval`` style syntax from cron
+#
+#   * See ``crontab(5)`` for additional details
+#
+#   * NOTE: This is overridden if ``$minute`` is set to anything other than
+#     ``nil`` or ``rand``.  If this is the case, it is assumed that you want
+#     finer control over your puppet run.
 #
 # @param minute_base
-#   The default artifact to use to auto-generate a cron interval.
+#   The default artifact to use to auto-generate a cron interval
 #
-#   The default of $::ipaddress is used to provide a reasonable guess at
-#   spreading the puppet runs across all of your systems. However, you
-#   can set this to *anything* that you like.
+#   * The default of ``$::ipaddress`` is used to provide a reasonable guess at
+#     spreading the puppet runs across all of your systems. However, you can
+#     set this to *anything* that you like.
 #
-#   Use $::ipaddress_eth0 to generate the entry from the eth0 IP Address
-#   Use $::uniqueid to generate the entry from the system UUID
+#   * Use ``$::ipaddress_eth0`` to generate the entry from the eth0 IP Address
 #
-#   If this is the *same* resolved value on all of your systems then
-#   your systems will have the *same* run interval.
+#   * Use ``$::uniqueid`` to generate the entry from the system UUID
+#
+#   * WARNING: If this is the *same* resolved value on all of your systems then
+#     your systems will have the *same* run interval.
 #
 # @param run_timeframe
-#   The time frame within which you wish to run the puppet agent. This
-#   directly translates to the minute field of the cron job so this
-#   should probably be left at 60.
+#   The time frame within which you wish to run the puppet agent
+#
+#   * This directly translates to the minute field of the cron job so this
+#     should probably be left at 60
 #
 # @param runs_per_timeframe
-#   The number of times, per $timeframe, that you want to run the Puppet
+#   The number of times, per ``$timeframe``, that you want to run the Puppet
 #   Agent.
 #
 # @param minute
-#   The 'minute' value for the crontab entry.
-#   Set to 'nil' if you want to use $interval.
+#   The ``minute`` value for the crontab entry
+#
+#   Set to ``nil`` if you want to use $interval
 #
 # @param hour
-#   The 'hour' value for the crontab entry.
-#   Not used if using $interval.
+#   The ``hour`` value for the crontab entry
+#
+#   Not used if using ``$interval``
 #
 # @param monthday
-#   The 'monthday' value for the crontab entry.
-#   Not used if using $interval.
+#   The ``monthday`` value for the crontab entry
+#
+#   * Not used if using ``$interval``
 #
 # @param month
-#   The 'month' value for the crontab entry.
-#   Not used if using $interval.
+#   The ``month`` value for the crontab entry
+#
+#   * Not used if using ``$interval``
 #
 # @param weekday
-#   The 'weekday' value for the crontab entry.
-#   Not used if using $interval.
+#   The ``weekday`` value for the crontab entry
+#
+#   * Not used if using ``$interval``
 #
 # @param maxruntime
 #   How long (in minutes) a puppet agent will be allowed to run before being
-#   forcibly stopped.  Defaults to 240 min (4 hours).
+#   forcibly stopped
 #
-# @param enable_agent
-#   If true, forcibly enable the puppet agent if it has been disabled for
-#   $max_disable_time.  If false, never force enable.
+# @param break_puppet_lock
+#   Forcibly enable the puppet agent if it has been disabled for
+#   ``$max_disable_time``
+#
+#   * This is enabled by default so that the system can remain in a
+#     self-healing state
 #
 # @param max_disable_time
 #   How long (in minutes) a puppet agent will be allowed to remain disabled
-#   before being forcibly enabled.  This only takes effect if $enable_agent
-#   is true.
+#   before being forcibly enabled
+#
+#   * This only takes effect if ``$break_puppet_lock`` is true
 #
 class pupmod::agent::cron (
   Integer[0]            $interval           = 30,
@@ -76,7 +88,7 @@ class pupmod::agent::cron (
   Variant[Array,String] $month              = '*',
   Variant[Array,String] $weekday            = '*',
   Integer[1]            $maxruntime         = 240,
-  Boolean               $enable_agent       = true,
+  Boolean               $break_puppet_lock  = true,
   Optional[Integer[1]]  $max_disable_time   = undef
 ) {
 
