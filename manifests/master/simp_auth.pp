d8cb8acbe6dbe541595dc6916793f7407663be24
Nick Miller <nick.miller@onyxpoint.com>
2017-05-30 17:05:23 -0500

    Always manage entries, fix docs

--- manifests/master/simp_auth.pp
+++ manifests/master/simp_auth.pp
@@ -5,51 +5,51 @@
 #   Default: /etc/puppetlabs/puppetserver/conf.d/auth.conf
 #   The location to the puppet master's auth.conf
 #
+# @param legacy_cacerts_all
+#   Type:    Boolean
+#   Default: true
+#   If enabled, allow access to the PKI cacerts from the legacy `pki` module from all hosts
+#
+# @param legacy_mcollective_all
+#   Type:    Boolean
+#   Default: true
+#   If enabled, allow access to the mcollective cacerts from the legacy `pki` module from all hosts
+#
+# @param legacy_pki_keytabs_from_host
+#   Type:    Boolean
+#   Default: true
+#   If enabled, allow access to each host's own kerberos keytabs from the legacy location
+#
 # @param pki_cacerts_all
 #   Type:    Boolean
 #   Default: true
-#   If enabled, allow PKI cacerts files from SIMP PKI module from all hosts
+#   If enabled, allow access to the cacerts from the `pki_files` module from all hosts
 #
 # @param pki_mcollective_all
 #   Type:    Boolean
 #   Default: true
-#   If enabled, allow SIMP site_files cacerts access from all hosts
-#
-# @param site_files_cacerts_all
-#   Type:    Boolean
-#   Default: true
-#   If enabled, allow MCO files from SIMP PKI module access from all hosts
-#
-# @param site_files_mcollective_all
-#   Type:    Boolean
-#   Default: true
-#   If enabled, allow SIMP site_files PKI mcollective access from all hosts
+#   If enabled, allow access to the mcollective PKI from the `pki_files` module from all hosts
 #
 # @param keydist_from_host
 #   Type:    Boolean
 #   Default: true
-#   If enabled, allow SIMP PKI keydist module access from specific host
-#
-# @param pki_keytabs_from_host
-#   Type:    Boolean
-#   Default: true
-#   If enabled, allow SIMP site_files PKI keytabs access from specific host
+#   If enabled, allow access to each host's own certs from the `pki_files` module
 #
 # @param krb5_keytabs_from_host
 #   Type:    Boolean
 #   Default: true
-#   If enabled, allow SIMP site_files krb5 keytabs access from specific host
+#   If enabled, allow access to each host's own kerberos keytabs from the `pki_files` module
 #
 class pupmod::master::simp_auth (
-  Simplib::ServerDistribution $server_distribution        = simplib::lookup('simp_options::puppet::server_distribution', { 'default_value' => 'PC1' } ),
-  Stdlib::AbsolutePath        $auth_conf_path             = '/etc/puppetlabs/puppetserver/conf.d/auth.conf',
-  Boolean                     $pki_cacerts_all            = true,
-  Boolean                     $pki_mcollective_all        = true,
-  Boolean                     $site_files_cacerts_all     = true,
-  Boolean                     $site_files_mcollective_all = true,
-  Boolean                     $keydist_from_host          = true,
-  Boolean                     $pki_keytabs_from_host      = true,
-  Boolean                     $krb5_keytabs_from_host     = true,
+  Simplib::ServerDistribution $server_distribution          = simplib::lookup('simp_options::puppet::server_distribution', { 'default_value' => 'PC1' } ),
+  Stdlib::AbsolutePath        $auth_conf_path               = '/etc/puppetlabs/puppetserver/conf.d/auth.conf',
+  Boolean                     $legacy_cacerts_all           = false,
+  Boolean                     $legacy_mcollective_all       = false,
+  Boolean                     $legacy_pki_keytabs_from_host = false,
+  Boolean                     $pki_cacerts_all              = true,
+  Boolean                     $pki_mcollective_all          = true,
+  Boolean                     $keydist_from_host            = true,
+  Boolean                     $krb5_keytabs_from_host       = true,
 ) {
 
   $_master_service = $server_distribution ? {
@@ -57,87 +57,86 @@ class pupmod::master::simp_auth (
     default => 'puppetserver',
   }
 
-  if $pki_cacerts_all {
-    puppet_authorization::rule { 'Allow PKI cacerts files from SIMP PKI module from all hosts':
-      match_request_path   => '^/puppet/v3/file_(metadata|content)/modules/pki/keydist/cacerts',
-      match_request_type   => 'regex',
-      match_request_method => ['get'],
-      allow                => '*',
-      sort_order           => 400,
-      path                 => $auth_conf_path,
-      notify               => Service[$_master_service],
-    }
+  # translates the parameter, which is a boolean, to the ensure parameter for the define
+  $bool2ensure = {
+    true  => 'present',
+    false => 'absent'
   }
 
-  if $site_files_cacerts_all {
-    puppet_authorization::rule { 'Allow SIMP site_files cacerts access from all hosts':
-      match_request_path   => '^/puppet/v3/file_(metadata|content)/site_files/pki_files/files/keydist/cacerts',
-      match_request_type   => 'regex',
-      match_request_method => ['get'],
-      allow                => '*',
-      sort_order           => 410,
-      path                 => $auth_conf_path,
-      notify               => Service[$_master_service],
-    }
+  puppet_authorization::rule { 'Allow access to the PKI cacerts from the legacy pki module from all hosts':
+    ensure               => $bool2ensure[$legacy_cacerts_all],
+    match_request_path   => '^/puppet/v3/file_(metadata|content)/modules/pki/keydist/cacerts',
+    match_request_type   => 'regex',
+    match_request_method => ['get'],
+    allow                => '*',
+    sort_order           => 400,
+    path                 => $auth_conf_path,
+    notify               => Service[$_master_service],
   }
 
-  if $pki_mcollective_all {
-    puppet_authorization::rule { 'Allow MCO files from SIMP PKI module access from all hosts':
-      match_request_path   => '^/puppet/v3/file_(metadata|content)/modules/pki/keydist/mcollective',
-      match_request_type   => 'regex',
-      match_request_method => ['get'],
-      allow                => '*',
-      sort_order           => 420,
-      path                 => $auth_conf_path,
-      notify               => Service[$_master_service],
-    }
+  puppet_authorization::rule { 'Allow access to the mcollective cacerts from the legacy pki module from all hosts':
+    ensure               => $bool2ensure[$legacy_mcollective_all],
+    match_request_path   => '^/puppet/v3/file_(metadata|content)/modules/pki/keydist/mcollective',
+    match_request_type   => 'regex',
+    match_request_method => ['get'],
+    allow                => '*',
+    sort_order           => 420,
+    path                 => $auth_conf_path,
+    notify               => Service[$_master_service],
   }
 
-  if $site_files_mcollective_all {
-    puppet_authorization::rule { 'Allow SIMP site_files PKI mcollective access from all hosts':
-      match_request_path   => '^/puppet/v3/file_(metadata|content)/site_files/pki_files/files/keydist/mcollective',
-      match_request_type   => 'regex',
-      match_request_method => ['get'],
-      allow                => '*',
-      sort_order           => 430,
-      path                 => $auth_conf_path,
-      notify               => Service[$_master_service],
-    }
+  puppet_authorization::rule { 'Allow access to each hosts own kerberos keytabs from the legacy location':
+    ensure               => $bool2ensure[$legacy_pki_keytabs_from_host],
+    match_request_path   => '^/puppet/v3/file_(metadata|content)/site_files/pki_files/files/keytabs/([^/]+)',
+    match_request_type   => 'regex',
+    match_request_method => ['get'],
+    allow                => '$2',
+    sort_order           => 450,
+    path                 => $auth_conf_path,
+    notify               => Service[$_master_service],
   }
 
-  if $keydist_from_host {
-    puppet_authorization::rule { 'Allow SIMP PKI keydist module access from specific host':
-      match_request_path   => '^/puppet/v3/file_(metadata|content)/modules/pki/keydist/([^/]+)',
-      match_request_type   => 'regex',
-      match_request_method => ['get'],
-      allow                => '$2',
-      sort_order           => 440,
-      path                 => $auth_conf_path,
-      notify               => Service[$_master_service],
-    }
+  puppet_authorization::rule { 'Allow access to the cacerts from the pki_files module from all hosts':
+    ensure               => $bool2ensure[$pki_cacerts_all],
+    match_request_path   => '^/puppet/v3/file_(metadata|content)/site_files/pki_files/files/keydist/cacerts',
+    match_request_type   => 'regex',
+    match_request_method => ['get'],
+    allow                => '*',
+    sort_order           => 410,
+    path                 => $auth_conf_path,
+    notify               => Service[$_master_service],
   }
 
-  if $pki_keytabs_from_host {
-    puppet_authorization::rule { 'Allow SIMP site_files PKI keytabs access from specific host':
-      match_request_path   => '^/puppet/v3/file_(metadata|content)/site_files/pki_files/files/keytabs/([^/]+)',
-      match_request_type   => 'regex',
-      match_request_method => ['get'],
-      allow                => '$2',
-      sort_order           => 450,
-      path                 => $auth_conf_path,
-      notify               => Service[$_master_service],
-    }
+  puppet_authorization::rule { 'Allow access to the mcollective PKI from the pki_files module from all hosts':
+    ensure               => $bool2ensure[$pki_mcollective_all],
+    match_request_path   => '^/puppet/v3/file_(metadata|content)/site_files/pki_files/files/keydist/mcollective',
+    match_request_type   => 'regex',
+    match_request_method => ['get'],
+    allow                => '*',
+    sort_order           => 430,
+    path                 => $auth_conf_path,
+    notify               => Service[$_master_service],
   }
 
-  if $krb5_keytabs_from_host {
-    puppet_authorization::rule { 'Allow SIMP site_files krb5 keytabs access from specific host':
-      match_request_path   => '^/puppet/v3/file_(metadata|content)/site_files/krb5_files/files/keytabs/([^/]+)',
-      match_request_type   => 'regex',
-      match_request_method => ['get'],
-      allow                => '$2',
-      sort_order           => 460,
-      path                 => $auth_conf_path,
-      notify               => Service[$_master_service],
-    }
+  puppet_authorization::rule { 'Allow access to each hosts own certs from the pki_files module':
+    ensure               => $bool2ensure[$keydist_from_host],
+    match_request_path   => '^/puppet/v3/file_(metadata|content)/modules/pki_files/keydist/([^/]+)',
+    match_request_type   => 'regex',
+    match_request_method => ['get'],
+    allow                => '$2',
+    sort_order           => 440,
+    path                 => $auth_conf_path,
+    notify               => Service[$_master_service],
+  }
+
+  puppet_authorization::rule { 'Allow access to each hosts own kerberos keytabs from the pki_files module':
+    ensure               => $bool2ensure[$krb5_keytabs_from_host],
+    match_request_path   => '^/puppet/v3/file_(metadata|content)/site_files/krb5_files/files/keytabs/([^/]+)',
+    match_request_type   => 'regex',
+    match_request_method => ['get'],
+    allow                => '$2',
+    sort_order           => 460,
+    path                 => $auth_conf_path,
+    notify               => Service[$_master_service],
   }
 }
--- spec/classes/master/simp_auth_spec.rb
+++ spec/classes/master/simp_auth_spec.rb
@@ -9,49 +9,56 @@ describe 'pupmod::master::simp_auth' do
 
       context 'with default parameters' do
         it { is_expected.to create_class('pupmod::master::simp_auth') }
-        it { is_expected.to create_puppet_authorization__rule('Allow PKI cacerts files from SIMP PKI module from all hosts').with({
+        it { is_expected.to create_puppet_authorization__rule('Allow access to the PKI cacerts from the legacy pki module from all hosts').with({
+          'ensure'               => 'absent',
           'match_request_path'   => '^/puppet/v3/file_(metadata|content)/modules/pki/keydist/cacerts',
           'match_request_type'   => 'regex',
           'match_request_method' => ['get'],
           'allow'                => '*',
           'sort_order'           => 400,
         }) }
-        it { is_expected.to create_puppet_authorization__rule('Allow SIMP site_files cacerts access from all hosts').with({
+        it { is_expected.to create_puppet_authorization__rule('Allow access to the cacerts from the pki_files module from all hosts').with({
+          'ensure'               => 'present',
           'match_request_path'   => '^/puppet/v3/file_(metadata|content)/site_files/pki_files/files/keydist/cacerts',
           'match_request_type'   => 'regex',
           'match_request_method' => ['get'],
           'allow'                => '*',
           'sort_order'           => 410,
         }) }
-        it { is_expected.to create_puppet_authorization__rule('Allow MCO files from SIMP PKI module access from all hosts').with({
+        it { is_expected.to create_puppet_authorization__rule('Allow access to the mcollective cacerts from the legacy pki module from all hosts').with({
+          'ensure'               => 'absent',
           'match_request_path'   => '^/puppet/v3/file_(metadata|content)/modules/pki/keydist/mcollective',
           'match_request_type'   => 'regex',
           'match_request_method' => ['get'],
           'allow'                => '*',
           'sort_order'           => 420,
         }) }
-        it { is_expected.to create_puppet_authorization__rule('Allow SIMP site_files PKI mcollective access from all hosts').with({
+        it { is_expected.to create_puppet_authorization__rule('Allow access to the mcollective PKI from the pki_files module from all hosts').with({
+          'ensure'               => 'present',
           'match_request_path'   => '^/puppet/v3/file_(metadata|content)/site_files/pki_files/files/keydist/mcollective',
           'match_request_type'   => 'regex',
           'match_request_method' => ['get'],
           'allow'                => '*',
           'sort_order'           => 430,
         }) }
-        it { is_expected.to create_puppet_authorization__rule('Allow SIMP PKI keydist module access from specific host').with({
-          'match_request_path'   => '^/puppet/v3/file_(metadata|content)/modules/pki/keydist/([^/]+)',
+        it { is_expected.to create_puppet_authorization__rule('Allow access to each hosts own certs from the pki_files module').with({
+          'ensure'               => 'present',
+          'match_request_path'   => '^/puppet/v3/file_(metadata|content)/modules/pki_files/keydist/([^/]+)',
           'match_request_type'   => 'regex',
           'match_request_method' => ['get'],
           'allow'                => '$2',
           'sort_order'           => 440,
         }) }
-        it { is_expected.to create_puppet_authorization__rule('Allow SIMP site_files PKI keytabs access from specific host').with({
+        it { is_expected.to create_puppet_authorization__rule('Allow access to each hosts own kerberos keytabs from the legacy location').with({
+          'ensure'               => 'absent',
           'match_request_path'   => '^/puppet/v3/file_(metadata|content)/site_files/pki_files/files/keytabs/([^/]+)',
           'match_request_type'   => 'regex',
           'match_request_method' => ['get'],
           'allow'                => '$2',
           'sort_order'           => 450,
         }) }
-        it { is_expected.to create_puppet_authorization__rule('Allow SIMP site_files krb5 keytabs access from specific host').with({
+        it { is_expected.to create_puppet_authorization__rule('Allow access to each hosts own kerberos keytabs from the pki_files module').with({
+          'ensure'               => 'present',
           'match_request_path'   => '^/puppet/v3/file_(metadata|content)/site_files/krb5_files/files/keytabs/([^/]+)',
           'match_request_type'   => 'regex',
           'match_request_method' => ['get'],
@@ -60,20 +67,10 @@ describe 'pupmod::master::simp_auth' do
         }) }
       end
 
-      context 'not managing one of the rules' do
-        let(:params) {{ :site_files_mcollective_all => false }}
-        it { is_expected.to create_puppet_authorization__rule('Allow PKI cacerts files from SIMP PKI module from all hosts') }
-        it { is_expected.to create_puppet_authorization__rule('Allow SIMP site_files cacerts access from all hosts') }
-        it { is_expected.to create_puppet_authorization__rule('Allow MCO files from SIMP PKI module access from all hosts') }
-        it { is_expected.not_to create_puppet_authorization__rule('Allow SIMP site_files PKI mcollective access from all hosts') }
-        it { is_expected.to create_puppet_authorization__rule('Allow SIMP PKI keydist module access from specific host') }
-        it { is_expected.to create_puppet_authorization__rule('Allow SIMP site_files PKI keytabs access from specific host') }
-        it { is_expected.to create_puppet_authorization__rule('Allow SIMP site_files krb5 keytabs access from specific host') }
-      end
-
       context 'on PE' do
         let(:params) {{ :server_distribution => 'PE' }}
-        it { is_expected.to create_puppet_authorization__rule('Allow SIMP site_files krb5 keytabs access from specific host').with({
+        it { is_expected.to create_puppet_authorization__rule('Allow access to each hosts own kerberos keytabs from the pki_files module').with({
+          'ensure'               => 'present',
           'match_request_path'   => '^/puppet/v3/file_(metadata|content)/site_files/krb5_files/files/keytabs/([^/]+)',
           'match_request_type'   => 'regex',
           'match_request_method' => ['get'],
