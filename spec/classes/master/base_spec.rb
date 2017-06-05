390ad6ad4cd8d87a2babdbd8c4fd0e9cb04bc5c9
Kendall Moore <kendall8688@gmail.com>
2017-06-05 16:27:05 -0400

    (SIMP-3049) SIMP should use the puppetlabs-puppet_authorization modul… (#57)
    - Added manifest to manage simp-specific puppet master auth requirements
    - Disabled puppetserver setting to enable legacy auth.conf by default
    - Remove legacy auth.conf placed by the `puppet-agent` package
    
    SIMP-3049 #comment This should complete the simp-pupmod portion of this ticket

--- spec/classes/master/base_spec.rb
+++ spec/classes/master/base_spec.rb
@@ -1,4 +1,3 @@
-#
 require 'spec_helper'
 
 describe 'pupmod::master::base' do
---
+++ spec/classes/master/simp_auth_spec.rb
@@ -0,0 +1,86 @@
+require 'spec_helper'
+
+describe 'pupmod::master::simp_auth' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) do
+        os_facts
+      end
+
+      context 'with default parameters' do
+        it { is_expected.to create_class('pupmod::master::simp_auth') }
+        it { is_expected.to create_puppet_authorization__rule('Allow access to the PKI cacerts from the legacy pki module from all hosts').with({
+          'ensure'               => 'absent',
+          'match_request_path'   => '^/puppet/v3/file_(metadata|content)/modules/pki/keydist/cacerts',
+          'match_request_type'   => 'regex',
+          'match_request_method' => ['get'],
+          'allow'                => '*',
+          'sort_order'           => 400,
+        }) }
+        it { is_expected.to create_puppet_authorization__rule('Allow access to the cacerts from the pki_files module from all hosts').with({
+          'ensure'               => 'present',
+          'match_request_path'   => '^/puppet/v3/file_(metadata|content)/pki_files/keydist/cacerts',
+          'match_request_type'   => 'regex',
+          'match_request_method' => ['get'],
+          'allow'                => '*',
+          'sort_order'           => 410,
+        }) }
+        it { is_expected.to create_puppet_authorization__rule('Allow access to the mcollective cacerts from the legacy pki module from all hosts').with({
+          'ensure'               => 'absent',
+          'match_request_path'   => '^/puppet/v3/file_(metadata|content)/modules/pki/keydist/mcollective',
+          'match_request_type'   => 'regex',
+          'match_request_method' => ['get'],
+          'allow'                => '*',
+          'sort_order'           => 420,
+        }) }
+        it { is_expected.to create_puppet_authorization__rule('Allow access to the mcollective PKI from the pki_files module from all hosts').with({
+          'ensure'               => 'present',
+          'match_request_path'   => '^/puppet/v3/file_(metadata|content)/pki_files/keydist/mcollective',
+          'match_request_type'   => 'regex',
+          'match_request_method' => ['get'],
+          'allow'                => '*',
+          'sort_order'           => 430,
+        }) }
+        it { is_expected.to create_puppet_authorization__rule('Allow access to each hosts own certs from the pki_files module').with({
+          'ensure'               => 'present',
+          'match_request_path'   => '^/puppet/v3/file_(metadata|content)/modules/pki_files/keydist/([^/]+)',
+          'match_request_type'   => 'regex',
+          'match_request_method' => ['get'],
+          'allow'                => '$2',
+          'sort_order'           => 440,
+        }) }
+        it { is_expected.to create_puppet_authorization__rule('Allow access to each hosts own kerberos keytabs from the legacy location').with({
+          'ensure'               => 'absent',
+          'match_request_path'   => '^/puppet/v3/file_(metadata|content)/pki_files/keytabs/([^/]+)',
+          'match_request_type'   => 'regex',
+          'match_request_method' => ['get'],
+          'allow'                => '$2',
+          'sort_order'           => 450,
+        }) }
+        it { is_expected.to create_puppet_authorization__rule('Allow access to each hosts own kerberos keytabs from the pki_files module').with({
+          'ensure'               => 'present',
+          'match_request_path'   => '^/puppet/v3/file_(metadata|content)/krb5_files/keytabs/([^/]+)',
+          'match_request_type'   => 'regex',
+          'match_request_method' => ['get'],
+          'allow'                => '$2',
+          'sort_order'           => 460,
+        }) }
+        it { is_expected.to create_file('/etc/puppetlabs/puppet/auth.conf').with_ensure('absent') }
+      end
+
+      context 'on PE' do
+        let(:params) {{ :server_distribution => 'PE' }}
+        it { is_expected.to create_puppet_authorization__rule('Allow access to each hosts own kerberos keytabs from the pki_files module').with({
+          'ensure'               => 'present',
+          'match_request_path'   => '^/puppet/v3/file_(metadata|content)/krb5_files/keytabs/([^/]+)',
+          'match_request_type'   => 'regex',
+          'match_request_method' => ['get'],
+          'allow'                => '$2',
+          'sort_order'           => 460,
+          'notify'               => 'Service[pe-puppetserver]'
+        }) }
+      end
+
+    end
+  end
+end
