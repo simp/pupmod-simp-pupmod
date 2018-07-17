8a7ac04741636aaab8f69e89bada7a476d2cc343
Brandon Riden <brandon.riden@onyxpoint.com>
2018-07-17 11:29:21 -0400

    moving pass_two to master_spec

--- spec/classes/master_spec.rb
+++ spec/classes/master_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
+require 'yaml'
+require 'pry'
+require 'pry-byebug'
 
 audit_content = File.open("#{File.dirname(__FILE__)}/data/auditd.txt", "rb").read;
+data = YAML.load_file("#{File.dirname(__FILE__ )}/data/moduledata.yaml")
 
 describe 'pupmod::master' do
   before :all do
@@ -378,25 +382,202 @@ describe 'pupmod::master' do
 
         describe "with non-default parameters" do
           context 'when server_distribution => PE' do
-            let(:params) {{:server_distribution => 'PE'}}
+            let(:params) {
+              {
+                  :server_distribution => 'PE'
+              }
+            }
 
             let(:facts){
               @extras.merge(os_facts).merge(
-                  :memorysize_mb => '490.16',
-                  :pe_build      => '2016.1.0'
+                :memorysize_mb => '490.16',
+                :pe_build      => '2016.1.0'
               )
             }
 
+            let(:pre_condition) {
+              'include ::pupmod'
+              'class { "pupmod::pass_two":
+                 server_distribution => "PE",
+                 firewall => true
+               }'
+              'include ::puppet_enterprise'
+            }
+
+            let(:title) { "main" }
+
             it 'sets $tmpdir via a pe_ini_subsetting resource' do
               expect(catalogue).to contain_pe_ini_subsetting('pupmod::master::sysconfig::javatempdir').with(
-                  'value' => %r{/pserver_tmp$},
-                  'path'  => '/etc/sysconfig/pe-puppetserver',
-                  )
+                'value' => %r{/pserver_tmp$},
+                'path'  => '/etc/sysconfig/pe-puppetserver',
+              )
             end
 
             it { is_expected.to contain_service('pe-puppetserver') }
             it { is_expected.not_to contain_service('puppetserver') }
+            {
+              'server' => {
+                'value' => '1.2.3.4'
+              },
+              'ca_server' => {
+                'value' => '$server'
+              },
+              'masterport' => {
+                'value' => 8140
+              },
+              'report' => {
+                'value' => false,
+                'section' => 'agent'
+              },
+              'ca_port' => {
+                'value' => 8141
+              }
+            }.each do |key, value|
+              it {is_expected.to_not contain_ini_setting("pupmod_#{key}")}
+            end
+            mode = nil
+            it {is_expected.to contain_file('/etc/puppetlabs/puppet').with({
+              'ensure' => 'directory',
+              'owner' => 'root',
+              'group' => 'puppet',
+              'mode' => mode,
+            })}
+            it {is_expected.to contain_file('/etc/puppetlabs/puppet/puppet.conf').with({
+              'ensure' => 'file',
+              'owner' => 'root',
+              'group' => 'puppet',
+              'mode' => mode
+            })}
+            it {is_expected.to contain_group('puppet').with({
+              'ensure' => 'present',
+              'allowdupe' => false,
+              'gid' => '52',
+              'tag' => 'firstrun',
+            })}
+
+            classlist = data['pupmod::pe_classlist'];
+            classlist.each do |key, value|
+              unless (key == 'pupmod' or key == 'pupmod::master')
+                context "when #{key} is included in the catalog" do
+                  let(:pre_condition) {
+                    if (key == 'puppet_enterprise::profile::master')
+                      ret = %{
+                      include puppet_enterprise
+                      class { 'pupmod':
+                        mock => true
+                      }
+                      include #{key}
+                      }
+                    else
+                      ret = %{
+                      include puppet_enterprise
+                      include pupmod
+                      include #{key}
+                      }
+                    end
+
+                    if defined?(data)
+                      _services = []
+                      data['pupmod::pe_classlist'].each_pair {|k, v|
+                        _services += v['services'] if v['services']
+                      }
+
+                      _services.uniq.each do |_service|
+                        ret << %{\nensure_resource('service', '#{_service}')}
+                      end
+                    end
+
+                    ret
+                  }
+
+                  users = value['users']
+                  unless (users == nil)
+                    users.each do |user|
+                      it "should contain Group[puppet] with user #{user} in the members array" do
+                        members = catalogue.resource('group', 'puppet').send(:parameters)[:members]
+                        expect(members.find {|x| x =~ Regexp.new("#{user}")}).to be_truthy
+                      end
+                    end
+                  end
+
+                  services = value['services']
+                  unless (services == nil)
+                    services.each do |service|
+                      it "should contain Group[puppet] that notifies Service[#{service}]" do
+                        notify = catalogue.resource('group', 'puppet').send(:parameters)[:notify]
+                        regex = Regexp.new("#{service}")
+                        expect(notify.find {|x| x.to_s =~ Regexp.new(regex)}).to be_truthy
+                      end
+                    end
+                  end
+                  firewall = value['firewall_rules']
+                  unless (firewall == nil)
+                    firewall.each do |rule|
+                      let(:params) {{'firewall' => true}}
+                      it {binding.pry;
+                      is_expected.to contain_iptables__listen__tcp_stateful("#{key} - #{rule['proto']} - #{rule['port']}").with({'dports' => rule['port']})}
+                    end
+                  end
+                end
+              end
+            end
           end
+
+          context "with pupmod::master defined" do
+            let(:pre_condition) {
+              '
+                include ::puppet_enterprise
+                include ::puppet_enterprise::profile::master
+                class { "::pupmod":
+                  mock => true
+                }
+                include pupmod::master
+              '
+            }
+            it {is_expected.to compile.and_raise_error(/.*pupmod::master is NOT supported on PE masters. Please remove the pupmod::master classification from hiera or the puppet console before proceeding.*/)}
+
+          end
+          context "with pupmod::master not defined" do
+            let(:pre_condition) {
+              '
+                include ::puppet_enterprise
+                include ::puppet_enterprise::profile::master
+                class { "::pupmod":
+                  mock => true
+                }
+              '
+            }
+            it {is_expected.to compile}
+            it {is_expected.to contain_class("pupmod::master::sysconfig")}
+            it {is_expected.to contain_class("pupmod::params")}
+            {
+                "2015.1.1" => true,
+                "2015.20.1" => true,
+                "2016.1.0" => true,
+                "2016.2.0" => true,
+                "2016.4.0" => false,
+                "2016.4.1" => false,
+                "2016.5.1" => false,
+                "2017.1.0" => false,
+                "2017.20.1" => false,
+                "2018.1.0" => false,
+                "2020.1.0" => false,
+                "2021.1.0" => false,
+            }.each do |pe_version, tmpdir|
+              it {is_expected.to contain_file("/opt/puppetlabs/server/data/puppetserver/pserver_tmp")}
+              context "when pe_version == #{pe_version}" do
+                let (:facts) do
+                  {"pe_build" => pe_version}.merge(facts)
+                end
+                if (tmpdir == true)
+                  it {is_expected.to contain_pe_ini_subsetting("pupmod::master::sysconfig::javatempdir")}
+                else
+                  it {is_expected.to_not contain_pe_ini_subsetting("pupmod::master::sysconfig::javatempdir")}
+                end
+              end
+            end
+          end
+
 
           context 'when server_distribution => PC1' do
             let(:params) {{:server_distribution => 'PC1'}}
@@ -405,21 +586,21 @@ describe 'pupmod::master' do
             puppetserver_content = File.open("#{File.dirname(__FILE__)}/master/data/puppetserver.txt", "rb").read
 
             it { is_expected.to contain_file('/etc/sysconfig/puppetserver').with(
-                {
-                    'owner'   => 'root',
-                    'group'   => 'puppet',
-                    'mode'    => '0640',
-                    'content' => puppetserver_content
-                }
+              {
+                'owner'   => 'root',
+                'group'   => 'puppet',
+                'mode'    => '0640',
+                'content' => puppetserver_content
+              }
             )}
             it { is_expected.to create_class('pupmod::master::sysconfig') }
             it { is_expected.to contain_file('/opt/puppetlabs/server/data/puppetserver/pserver_tmp').with(
-                {
-                    'owner'  => 'puppet',
-                    'group'  => 'puppet',
-                    'ensure' => 'directory',
-                    'mode'   => '0750'
-                }
+              {
+                'owner'  => 'puppet',
+                'group'  => 'puppet',
+                'ensure' => 'directory',
+                'mode'   => '0750'
+              }
             )}
 
             it { is_expected.to contain_service('puppetserver') }
@@ -453,9 +634,9 @@ describe 'pupmod::master' do
                 'notify'  => 'Service[puppetserver]'
               }) }
 
-                it { expect(ca_cfg_lines).to eq ([
-                  'puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service'
-                ]) }
+              it { expect(ca_cfg_lines).to eq ([
+                'puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service'
+              ]) }
             end
           end
 
@@ -534,9 +715,9 @@ describe 'pupmod::master' do
 
           context 'with non-empty ruby_load_path' do
             let(:params) {{:ruby_load_path => '/some/ruby/path'}}
-              context 'when processing os-settings.conf' do
-                let(:os_settings_conf) { '/etc/puppetlabs/puppetserver/conf.d/os-settings.conf' }
-                let(:os_settings_conf_hash) { Hocon.parse(catalogue.resource("File[#{os_settings_conf}]")['content']) }
+            context 'when processing os-settings.conf' do
+              let(:os_settings_conf) { '/etc/puppetlabs/puppetserver/conf.d/os-settings.conf' }
+              let(:os_settings_conf_hash) { Hocon.parse(catalogue.resource("File[#{os_settings_conf}]")['content']) }
 
               it { is_expected.to contain_file(os_settings_conf).with({
                 'ensure'  => 'file',
@@ -553,7 +734,7 @@ describe 'pupmod::master' do
                   'ruby-load-path' => [params[:ruby_load_path]].flatten
                 )
               }
-              end
+            end
           end
 
           context 'with empty ssl_protocols, non-empty ssl_cipher_suites, and multiple admin_api_whitelist entries' do
