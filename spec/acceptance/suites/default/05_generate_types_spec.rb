db052f3e4624a03295babc2da70f90d8cc3569e4
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-11-07 17:04:13 -0500

    Add explicit timeouts to acceptance tests

--- spec/acceptance/suites/default/05_generate_types_spec.rb
+++ spec/acceptance/suites/default/05_generate_types_spec.rb
@@ -10,7 +10,8 @@ describe 'incron driven puppet generate types'  do
 
       it 'should have run `puppet generate types`' do
         # Generate types sleeps for 30 seconds by default
-        retry_on(host, "ls -al #{incron_cache}", :max_retries => 35)
+        sleep(35)
+        on(host, "ls -al #{incron_cache}")
       end
 
       it 'should not recreate the resource cache after deletion' do
@@ -21,14 +22,16 @@ describe 'incron driven puppet generate types'  do
       it 'should create the resource cache in a new environment' do
         on(host, "cp -ra #{environment_path}/production #{environment_path}/new_environment")
         # Give it some time to generate everything
-        retry_on(host, "ls -al #{environment_path}/new_environment/.resource_types", :max_retries => 35)
+        sleep(35)
+        on(host, "ls -al #{environment_path}/new_environment/.resource_types")
       end
 
       it 'should recreate the resource cache if a library is updated' do
         on(host, "echo '' >> #{environment_path}/production/modules/incron/lib/puppet/type/incron_system_table.rb")
 
         # Give it some time to update
-        retry_on(host, "ls -al #{incron_cache}", :max_retries => 35)
+        sleep(35)
+        on(host, "ls -al #{incron_cache}")
       end
     end
   end
