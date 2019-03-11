9963663aa093974274e8bb21d0cede62398246db
jeannegreulich <jeanne.greulich@onyxpoint.com>
2019-03-11 16:57:38 -0400

    change fact to list of jruby files

---
+++ lib/facter/puppetserver_jruby.rb
@@ -0,0 +1,17 @@
+# Return a list of jruby jar files in the installation directory
+#
+Facter.add('puppetserver_jruby') do
+  confine do
+    File.directory?('/opt/puppetlabs/server/apps/puppetserver') && File.readable?('/opt/puppetlabs/server/apps/puppetserver')
+  end
+
+  setcode do
+    jruby_hash = {
+      'dir' => '/opt/puppetlabs/server/apps/puppetserver',
+      'jarfiles' => []
+    }
+    jarfiles = Dir.glob("#{jruby_hash['dir']}/*.jar")
+    jruby_hash['jarfiles'] = jarfiles.map { |x| x.split('/').last}
+    jruby_hash
+  end
+end
