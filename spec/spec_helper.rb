666b9251be572b399eabcef4bcd2cde8fdc1fae7
Brandon Riden <brandon.riden@onyxpoint.com>
2019-06-26 16:31:29 -0400

    Revert hiera, fix enforce spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,8 +20,6 @@ default_hiera_config =<<-EOM
 ---
 version: 5
 hierarchy:
-  - name: Hostname
-    path: "%{facts.hostname}.yaml"
   - name: SIMP Compliance Engine
     lookup_key: compliance_markup::enforcement
     options:
